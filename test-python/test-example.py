cd2423a7df9a10700277e5c68c24f653590e5cce
Oliver Black <oliver.black.uk@gmail.com>
2019-01-29 11:16:47 +0000

    Fix IPv6 address allocation

--- test-python/test-example.py
+++ test-python/test-example.py
@@ -13,23 +13,41 @@ class ExampleTopo(Topo):
         # Add hosts and switches
         left_host = self.addHost('h1')
         right_host = self.addHost('h2')
-        left_switch = self.addSwitch('s3')
-        right_switch = self.addSwitch('s4')
+        switch = self.addSwitch('s3')
 
         # Add links
-        self.addLink(left_host, left_switch)
-        self.addLink(left_switch, right_switch)
-        self.addLink(right_switch, right_host)
+        self.addLink(left_host, switch)
+        self.addLink(switch, right_host)
+
+
+def setup_ipv6(net, node_ids, switch_ids):
+    addresses = {}
+    for switch_id in switch_ids :
+        switch = net.get(switch_id)
+        switch.cmd("sysctl net.ipv6.conf.all.disable_ipv6=0")
+
+    count = 1
+    for node_id in node_ids :
+        node = net.get(node_id)
+        address = "fc00::"+str(count)
+        node.cmd("ifconfig "+node_id+"-eth0 inet6 add "+address+"/64")
+        addresses[node_id] = address
+        count = count + 1
+
+    return addresses
 
 
 def run():
     topo = ExampleTopo()
     net = Mininet(topo=topo)
     net.start()
+    ipv6_addresses = setup_ipv6(net, {'h1','h2'}, {'s3'})
     info('Example test starting\n')
     node = net.get('h1')
     node2 = net.get('h2')
-    node.cmdPrint("ping -w 2 "+node2.IP())
+    print(ipv6_addresses[node2.name])
+    node2.cmdPrint("ifconfig")
+    node.cmdPrint("ping6 -w 2 "+ipv6_addresses[node2.name])
     info('Example test completed\n')
     net.stop()
 
