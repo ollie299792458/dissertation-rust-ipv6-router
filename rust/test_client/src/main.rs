a9866ea3efbaa51333fe6e77aff312d69f717055
Oliver Black <oliver.black.uk@gmail.com>
2019-04-21 23:36:45 +0100

    Backup

--- rust/test_client/src/main.rs
+++ rust/test_client/src/main.rs
@@ -3,22 +3,24 @@ use std::env;
 use pnet::datalink::{self, NetworkInterface};
 use pnet::datalink::Channel::Ethernet;
 use pnet::datalink::{DataLinkReceiver, DataLinkSender};
-use pnet::packet::MutablePacket;
+use pnet::packet::{Packet,MutablePacket};
 use pnet::packet::ethernet::MutableEthernetPacket;
 use pnet::packet::ethernet::EtherTypes::Ipv6;
+use pnet::packet::ip::IpNextHeaderProtocol;
 use pnet::packet::ipv6::MutableIpv6Packet;
 use std::net::Ipv6Addr;
+use pnet::util::MacAddr;
 
 fn main() {
     let interface_name = env::args().nth(1).unwrap();
 
-    let test = evn::args().nth(2).unwrap();
+    let test = env::args().nth(2).unwrap();
 
-    let destination_mac = env::args().nth(3).unwrap();
+    let destination_mac = env::args().nth(3).unwrap().parse::<MacAddr>().unwrap();
 
-    let source_ip = env::args().nth(4).unwrap();
+    let source_ip = env::args().nth(4).unwrap().parse::<Ipv6Addr>().unwrap();
 
-    let destination_ip = env::args().nth(5).unwrap();
+    let destination_ip = env::args().nth(5).unwrap().parse::<Ipv6Addr>().unwrap();
 
     println!("Client starting");
 
@@ -32,7 +34,7 @@ fn main() {
         .next()
         .unwrap();
 
-    let (tx, mut rx) = match datalink::channel(&interface, Default::default()) {
+    let (mut tx, rx) = match datalink::channel(&interface, Default::default()) {
         Ok(Ethernet(itx, irx)) => (itx, irx),
         Ok(_) => panic!("Unhandled channel type"),
         Err(e) => panic!("An error occurred when creating the datalink channel: {}", e)
@@ -42,23 +44,34 @@ fn main() {
     let mut buffer:Vec<u8> = vec![0;size];
     let mut packet= MutableEthernetPacket::new(&mut buffer).unwrap();
 
-    let mut payload = Ipv6Packet::new(&mut packet.payload_mut());
-    payload.set_version(6);
-    payload.set_traffic_class(0);
-    payload.set_flow_label(0);
-    payload.set_payload_length();
-    payload.set_next_header(58);
-    payload.set_hop_limit(10);
-    payload.set_source_address(source_ip.parse::<Ipv6Addr>());
-    payload.set_destination_address(destination_ip.parse::<Ipv6Addr>());
+    let mut payload = MutableIpv6Packet::new(packet.payload_mut()).unwrap();
+    get_ipv6_packet(&mut payload, source_ip, destination_ip);
+    if test == "11211" {
+        get_11211_packet(&mut payload)
+    }
 
-    packet.set_source_address(interface.mac_address);
-    packet.set_destination_address(destination_mac);
+    packet.set_source(interface.mac_address());
+    packet.set_destination(destination_mac);
     packet.set_ethertype(Ipv6);
 
-    tx.send_to(&packet, None);
+    tx.send_to(&packet.packet(), None);
 
     println!("Packets sent");
 }
 
-fn
+fn get_11211_packet(packet: &mut MutableIpv6Packet) {
+    packet.set_payload_length(4);
+    let payload:Vec<u8> = vec![1,2,3,4];
+    packet.set_payload(&payload);
+}
+
+fn get_ipv6_packet(packet: &mut MutableIpv6Packet, source:Ipv6Addr, destination:Ipv6Addr) {
+    packet.set_version(6);
+    packet.set_traffic_class(0);
+    packet.set_flow_label(0);
+    packet.set_payload_length(0);
+    packet.set_next_header(IpNextHeaderProtocol::new(58));
+    packet.set_hop_limit(10);
+    packet.set_source(source);
+    packet.set_destination(destination);
+}
